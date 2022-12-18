0b59a26d9a6f45a6222e6fd6b151ed179326e114
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2022-12-19 00:57:25 +0100

    Add files via upload

---
+++ Chapter1questions.py
@@ -0,0 +1,56 @@
+#Q1 Which of the following are operators, and which are values?
+
+# * -operator
+# 'hello' -value
+# -88.8 -value
+# - -operator
+# / -operator
+# + -operator
+# 5 -value
+
+
+#Q2 Which of the following is  variable, and which is a string?
+
+# spam - variable 
+# 'spam' - string
+
+#Q3 Name three data types. 
+
+# The three data types are: integers, strings and floats. 
+
+#Q4 What is an expression made up of? What do all expressions do?
+
+#An expression is made up of operators, variables and function calls. All expressions are elements that form a program as a whole. They also evaluate, aka reduce to a single value. 
+
+#Q5 This chapter introduced assignment statements, like spam=10. What's the difference between an expression and a statement?
+
+# An expression evaluates (reduces to a single value). A statement does not do this. 
+
+#Q6 What does the variable bacon contain after the following code runs?
+
+# bacon=20
+# bacon+1
+
+# The variable bacon contains 21. 
+
+#Q7 What should the following two expressions evaluate to?
+
+# 'spam' + 'spamspam'
+# 'spam'*3
+
+# Both expressions evaluate to 'spamspamspam'. 
+
+#Q8 Why is eggs a valid variable name while 100 is invalid?
+
+# Because variable name cannot begin with a number.
+
+#Q9 What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+# The functions int (), float () and str ().
+
+#Q10 Why does this expression cause an error? How can you fix it?
+
+# 'I have eaten' + 99 + 'burritos.'
+
+# The problem is that 99 is an integer and it cannot be concatenated to a string. To correct it we must write 'I have eaten' + str (99) + 'burritos.'
+
