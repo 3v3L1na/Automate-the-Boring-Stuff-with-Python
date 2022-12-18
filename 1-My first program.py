ab18959c9ae9a0d0a4a3e867a2a4140441de157f
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2022-12-19 00:27:39 +0100

    Add files via upload

---
+++ 1-My first program.py
@@ -0,0 +1,11 @@
+print ('Hello world!')
+print ('What is your name?')
+myName=input ('Ena')
+print ('It is good to meet you', myName)
+
+print ('The length of your name is:')
+print (len(myName))
+
+print ('What is your age?')
+myAge=input (37)
+print ('Oh, wow. You will be '+str(int(myAge)+1)+ 'in a year.')
