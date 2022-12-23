b42d301c03c2a61dab5c948183e4b3c9b1451fe2
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2022-12-23 19:34:24 +0100

    Add files via upload

---
+++ 2-vampire.py
@@ -0,0 +1,11 @@
+name='Evelyn'                                                       # Since the name is not 'Alice', an elif is executed, one of the three that fulfils the condition. 
+age=2014
+if name== 'Alice':
+    print ('Hi Alice')
+    
+elif age <12:
+    print ('You are not Alice, kiddo')
+elif age >2000:
+    print ('Unlike you, Alice is not an undead, immortal vampire.')
+elif age >100: 
+    print ('You are not Alice, grannnie')
